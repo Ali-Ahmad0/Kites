762e7d493cf00adc6583faf4e9953de282efed23
Abeeha5 <abeehaturi1214@gmail.com>
2025-03-01 22:50:34 +0500

    Fixed the sidebar

--- src/lib/navigation/sidebar.svelte
+++ src/lib/navigation/sidebar.svelte
@@ -1,15 +1,24 @@
 <script>
-
 	let sidebarCollapsed = false;
-	
+
 	function toggleSidebar() {
-		sidebarCollapsed = !sidebarCollapsed;
+		sidebarCollapsed = !sidebarCollapsed; 
 	}
+
+	function checkWindowSize() {
+		if (window.innerWidth <= 1024) {
+			sidebarCollapsed = true;
+		} else {
+			sidebarCollapsed = false;
+		}
+	}
+	window.addEventListener('resize', checkWindowSize);
+	window.addEventListener('load', checkWindowSize);
 
 </script>
 
 <div class="sidebar" class:collapsed={sidebarCollapsed}>
-	<button on:click={toggleSidebar} >
+	<button on:click={toggleSidebar}>
 		☰
 	</button>
 	<div class="menu-links">
@@ -32,26 +41,29 @@
 		position: fixed;
 		left: 0;
 		top: 0;
-		height: 100vh;
-		width: 8vw;
+		height: 100%;
+		width: 160px; 
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		padding: 2.5%;
-		margin-top: 5%;
-		transition: width 0.3s ease;
+		top: 10%;
+		transition: transform 0.3s ease; 
+		overflow: hidden;
+		z-index: 100;
+		box-shadow: 2px 0 10px rgba(0, 0, 0, 0.5);
 	}
 
 	.sidebar.collapsed {
-		width: 0;
+		transform: translateX(-78%);
 	}
 
 	.sidebar::before {
 		content: "";
 		position: absolute;
 		right: 0;
-		top: 0;
-		height: 100%;
+		top: 0; 
+		height: 100%; 
 		width: 1px;
 		background: rgba(255, 255, 255, 0.5);
 	}
@@ -65,28 +77,31 @@
 	}
 
 	.sidebar a {
-
 		color: var(--color-text-primary);
 		font-size: 1rem;
 		text-decoration: none;
 		transition: opacity 0.3s ease;
 	}
 
-	.sidebar.collapsed a {
-		display: none; 
-	}
-
 	.sidebar a:hover {
 		color: var(--color-text-secondary);
+        background-color: var(--color-background-primary);
+        transition: color 0.3s ease 0s;
 	}
 
 	button {
-		position: relative;
-		background: none;
-		border: none;
-		color: var(--color-text-primary);
-		font-size: 1.5rem;
-		cursor: pointer;
+	background: none;
+	border: none;
+	color: var(--color-text-primary);
+	font-size: 1.5rem;
+	cursor: pointer;
+	margin-bottom: 10px;
+	display: flex;
+	justify-content: center; 
+	align-items: center;
+	position: relative;
+	width: 100%; 
+	padding-left: 170px;
 	}
 
 </style>
