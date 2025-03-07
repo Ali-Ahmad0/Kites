fc7c694423bd9d443e64a113e9308b320a16e7c7
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2025-03-07 11:47:45 +0500

    fixed stuff

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
-import { DB_URL } from "$env/static/private";
-
-const client = new MongoClient(DB_URL);
+import { DATABASE_URL } from "$env/static/private";
+ 
+const client = new MongoClient(DATABASE_URL);
 
 export function db_connect() {
     console.log("[KITES | INFO]: Connecting to mongodb...");
