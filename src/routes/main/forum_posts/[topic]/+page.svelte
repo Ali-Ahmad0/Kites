a2de006c486f1dddbb93ae3e28fffdba0ccdfe2e
Ali-Ahmad0 <aliahmadbhs@gmail.com>
2025-05-12 15:20:35 +0500

    loading skeleton

--- src/routes/main/forum_posts/[topic]/+page.svelte
+++ src/routes/main/forum_posts/[topic]/+page.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import { Thumbnail, Featured } from '$lib';
+	import { Thumbnail, Featured, Loading, LoadingMore } from '$lib';
 	import { onMount, onDestroy } from 'svelte';
 
     const { data } = $props();
 
-    let posts = $state(data.posts);
+    let posts : any = $state([]);
     let current_page = $state(1);
-
+    
     let is_loading = $state(false);
     let has_more_posts = $state(true);
+    
+    // Handle streamed data when it resolves
+    $effect(() => {
+        if (data.streamed) {
+            data.streamed.then(streamed_data => {
+                posts = streamed_data.posts;
+            });
+        }
+    });
 
     async function load_more_posts() {
         if (is_loading || !has_more_posts) 
@@ -38,7 +47,7 @@
 
     // Intersection Observer to detect when user scrolls to bottom
     let observer: IntersectionObserver | null;
-    let sentinel: HTMLDivElement;
+    let sentinel: HTMLDivElement | undefined = $state(undefined);
 
     onMount(() => {
         observer = new IntersectionObserver((entries) => {
@@ -63,14 +72,19 @@
     });
 </script>
 
-{#each data.posts as post }
-    <Thumbnail post_id={post.id} username={post.author_name} pfp={post.author_pfp}
-    topic={post.topic} user_liked={post.user_liked} heading={post.heading} image={post.image_url} type={post.type}/>
-{/each}
-<div bind:this={sentinel}></div>
+{#await data.streamed}
+    <Loading text="Loading posts..."/>
+{:then posts_data} 
+    {#each posts as post}
+        <Thumbnail post_id={post.id} username={post.author_name} pfp={post.author_pfp}
+        topic={post.topic} user_liked={post.user_liked} heading={post.heading} image={post.image_url} type={post.type}/>
+    {/each}
 
-{#if is_loading}
-    <p>Loading...</p>
-{/if}
+    <div bind:this={sentinel}>
+        {#if is_loading}
+            <LoadingMore/>
+        {/if}
+    </div>
 
-<Featured posts={data.featured_posts} />
+    <Featured posts={posts_data.featured_posts} />
+{/await}
--- src/routes/main/user/[username]/+page.server.ts
+++ src/routes/main/user/[username]/+page.server.ts
@@ -5,40 +5,45 @@ export async function load({ params } : any) {
     // Access username from route parameters
     const { username } = params;
     
-    const user = await prisma.users.findUnique({
-        where: { username: username },
-        select: {
-            username: true,
-            rank: true,
-            email: true,
-            image: {
-                select: { binary_blob: true, mime_type: true }
+    const streamed = (async () => { 
+        const user = await prisma.users.findUnique({
+            where: { username: username },
+            select: {
+                username: true,
+                rank: true,
+                email: true,
+                image: {
+                    select: { binary_blob: true, mime_type: true }
+                }
             }
+        })
+        
+        // Throw a 404 error if the user does not exist
+        if (!user) {
+            throw error(404, {
+                message: 'User not found'
+            });
         }
-    })
-    
-    // Throw a 404 error if the user does not exist
-    if (!user) {
-        throw error(404, {
-            message: 'User not found'
-        });
-    }
-    
-    const email = user.email;
-    const rank = user.rank;
-    
-    let image_url = null;
-    if (user.image) {
-        // Convert Prisma Bytes (Buffer) to base64
-        const base64 = Buffer.from(user.image.binary_blob).toString('base64');
-        image_url = `data:${user.image.mime_type};base64,${base64}`;
-    }
-    
+        
+        const email = user.email;
+        const rank = user.rank;
+        
+        let image_url = null;
+        if (user.image) {
+            // Convert Prisma Bytes (Buffer) to base64
+            const base64 = Buffer.from(user.image.binary_blob).toString('base64');
+            image_url = `data:${user.image.mime_type};base64,${base64}`;
+        }
+
+        return {
+            email: user.email,
+            rank: user.rank,
+            image: image_url
+        };
+    })();
     // Return username and email from parameters
     return {
         params_username: username,
-        params_email_id: email,
-        user_rank: rank,
-        image: image_url
+        streamed
     };
 }
